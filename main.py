4f3abe1bf31eb2bb717d72ad6111907a630c3fe4
SuyogJo <60322402+SuyogJo@users.noreply.github.com>
2021-05-05 19:56:54 -0400

    Loops and Plot trendline for better data analysis
    Added Loops to only include 0 ETH transactions and added a trendline to see a better correlation to the data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 url = 'https://etherscan.io/txs'
 
@@ -11,17 +12,43 @@ header = {
 
 r = requests.get(url, headers=header)
 
-dfs = pd.read_html(r.text, index_col=2)
 
-print(dfs)
+nonZeroValue = []
+nonZeroGas = []
 
-df = dfs[0]
-df.head()
 
-df = df.replace({'Ether': ''}, regex=True)
-df['Value'] = df['Value'].apply(pd.to_numeric)
-df.info()
+
+while len(nonZeroValue) <= 50:
+  dfs = pd.read_html(r.text, index_col=2)
+  #print(dfs)
+  df = dfs[0]
+  df.head()
+  df = df.replace({'Ether': ''}, regex=True)
+  df['Value'] = df['Value'].apply(pd.to_numeric)
+  transacValue = (df['Value'])
+  gasPrice = (df['Unnamed: 9'])
+
+  for i in range(len(transacValue)):
+    if (transacValue[i] != 0 and transacValue[i] <= 3):
+      nonZeroValue.append(transacValue[i])
+      nonZeroGas.append(gasPrice[i])
+    else:
+      pass
+
+print(nonZeroValue)
+print(nonZeroGas)
+
+
+nonZeroVal = np.asarray(nonZeroValue)
+nonZeroG = np.asarray(nonZeroGas)
+
+m, b = np.polyfit(nonZeroValue, nonZeroGas, 1)
 
 plt.style.use('seaborn-whitegrid')
-df.plot.bar(x='Value', y=['Unnamed: 9'])
+plt.scatter(nonZeroVal, nonZeroG)
+plt.plot(nonZeroVal, m*nonZeroVal + b)
+plt.title("The relationship between the transaction value and the gas prices \nnot including 0 ETH transactions")
+plt.xlabel("Values of the Transaction in ETH")
+plt.ylabel("Gas Values in ETH")
 plt.show()
+
